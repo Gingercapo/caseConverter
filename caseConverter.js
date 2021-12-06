a624c02fc3d8a42b58d56136005e029b3d35ec74
Obianonwo Chinedu <56441231+Gingercapo@users.noreply.github.com>
2021-12-06 15:14:39 +0100

    Create caseConverter.js

---
+++ caseConverter.js
@@ -0,0 +1,59 @@
+let upper = document.getElementById("upper-case");
+let lower = document.getElementById("lower-case");
+let proper = document.getElementById("proper-case");
+let sentence = document.getElementById("sentence-case");
+let SaveTextFile = document.getElementById("save-text-file");
+
+upper.addEventListener("click", function () {
+    let text = document.querySelector("textarea");
+    text.value = text.value.toUpperCase();
+});
+
+lower.addEventListener("click", function () {
+    let text = document.querySelector("textarea");
+    text.value = text.value.toLowerCase();
+});
+
+proper.addEventListener("click", function () {
+    let text = document.querySelector("textarea");
+    let words = text.value.split(" ");
+    upperFirst(words);
+    text.value = words.join(" ");
+});
+
+sentence.addEventListener("click", function () {
+    let text = document.querySelector("textarea");
+    let words = text.value.split(/\. /);
+    upperFirst(words);
+    text.value = words.join(". ");
+});
+
+SaveTextFile.addEventListener("click", function () {
+    let text = document.getElementById("text").value;
+
+
+    download("text.txt", text,);
+},false);
+
+
+
+     function upperFirst(words) {
+    for (let i = 0; i < words.length; i++) {
+        let word = words[i];
+        words[i] = word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase();
+    }
+}
+
+     function download(filename, text) {
+        var element = document.createElement('a');
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+        element.setAttribute('download', filename);
+
+        element.style.display = 'none';
+        document.body.appendChild(element);
+
+        element.click();
+
+        document.body.removeChild(element);
+    }
+
